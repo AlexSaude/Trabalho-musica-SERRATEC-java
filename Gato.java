6a7aec54dc3bcc8b0b86034e791e7cb5ab8a2843
AlexSaude <46931116+AlexSaude@users.noreply.github.com>
2025-09-16 20:11:42 -0300

    Add files via upload

---
+++ Gato.java
@@ -0,0 +1,20 @@
+package trabalhoMusica;
+
+public class Gato {
+private String nome;
+private String sexo;
+
+
+public Gato() {
+	// TODO Auto-generated constructor stub
+}
+
+public void emitirSom() {
+	System.out.println("la em casa tinha um gato");
+	System.out.println("la em casa tinha um gato");
+}
+
+public String Tostring() {
+	return "E o gato miau";
+}
+}
