fc37b1530d944be66f92dc2428ec5dad329b577f
AlexSaude <46931116+AlexSaude@users.noreply.github.com>
2025-09-16 20:08:42 -0300

    Add files via upload

---
+++ Galinha.java
@@ -0,0 +1,20 @@
+package trabalhoMusica;
+
+public class Galinha extends Animal{
+public Galinha(String animal, String som) {
+		super(animal, som);
+		// TODO Auto-generated constructor stub
+	}
+
+private String nome;
+private String sexo;
+
+public void emitirSom() {
+	System.out.println("la em casa tinha uma galinha");
+	System.out.println("la em casa tinha uma galinha");
+}
+public String Tostring() {
+	return "e a galinha có";
+}
+
+}
