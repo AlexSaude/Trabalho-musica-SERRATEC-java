125f2db04541116e8b6b5012617248a0ce798348
AlexSaude <46931116+AlexSaude@users.noreply.github.com>
2025-09-16 20:00:34 -0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,20 @@
+package trabalhoMusica;
+
+
+
+public class Animal {
+	private String animal;
+	private String som;
+	
+	public Animal(String animal, String som) {
+		super();
+		this.animal = animal;
+		this.som = som;
+	}
+
+	public void emitirSom(int flag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
