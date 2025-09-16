b4fa1dae9305863094b096403022cd5214c3ec0b
AlexSaude <46931116+AlexSaude@users.noreply.github.com>
2025-09-16 20:04:39 -0300

    Classe Cachorro
    classe Cachorro do package trabalhoMusica

---
+++ Cachorro.java
@@ -0,0 +1,22 @@
+package trabalhoMusica;
+
+public class Cachorro {
+private String nome;
+private String sexo;
+public Cachorro(String nome, String sexo) {
+	super();
+	this.nome = nome;
+	this.sexo = sexo;
+}
+public Cachorro() {
+	// TODO Auto-generated constructor stub
+}
+
+public void emitirSom() {
+	System.out.println("la em casa tinha um cachorro");
+	System.out.println("la em casa tinha um cachorro");
+}
+public String Tostring() {
+	return "E o cachorro au au";
+}
+}
